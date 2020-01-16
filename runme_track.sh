85e3ced759edffe1d14a42209c63ac5b3227979b
s1613134 <s1613134@u.tsukuba.ac.jp>
2020-01-16 18:48:40 +0900

    update

--- runme_track.sh
+++ runme_track.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-<< COMMENTOUT1
-pushd `dirname $0` > /dev/null
-SCRIPTPATH=`pwd -P`
-popd > /dev/null
-BEDPOSTDIR=$SCRIPTPATH/bedpost
-BEDPOSTOUTDIR=$BEDPOSTDIR.bedpostX
 
-rm -rf $BEDPOSTOUTDIR
+export TUTORIAL_DATA=~/freesurfer_rta;export SUBJECTS_DIR=$TUTORIAL_DATA/reconall2020;cd $TUTORIAL_DATA/tutorial
+<<COMMENTOUT1
 echo "fsl_sub running $COMMANDNAME with parallel processing disabled"
 FSLPARALLEL=0; export FSLPARALLEL
-time bedpostx $BEDPOSTDIR
-
-rm -rf $BEDPOSTOUTDIR
+time trac-all -bedp -c ~/git/tr_accel/dmrirc.rta
 COMMENTOUT1
 echo "fsl_sub running $COMMANDNAME with parallel processing enabled"
 FSLPARALLEL=1; export FSLPARALLEL
-export TUTORIAL_DATA=~/freesurfer_tutorial/tutorial_data_20190918_1558;export SUBJECTS_DIR=$TUTORIAL_DATA/diffusion_recons;cd $TUTORIAL_DATA/diffusion_tutorial
-trac-all -bedp -c $TUTORIAL_DATA/../dmrirc.tutorial2
+time trac-all -bedp -c ~/git/tr_accel/dmrirc.rta
 #time bedpostx $BEDPOSTDIR
 
