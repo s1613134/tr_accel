7caf7dff25bbabc9e7b736cf4bfabfc26ad788fe
s1613134 <s1613134@u.tsukuba.ac.jp>
2020-01-16 23:52:06 +0900

    update

--- runme_track.sh
+++ runme_track.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+export TUTORIAL_DATA=~/freesurfer_rta;export SUBJECTS_DIR=$TUTORIAL_DATA;cd $SUBJECTS_DIR
 
-export TUTORIAL_DATA=~/freesurfer_rta;export SUBJECTS_DIR=$TUTORIAL_DATA/reconall2020;cd $TUTORIAL_DATA/tutorial
-<<COMMENTOUT1
-echo "fsl_sub running $COMMANDNAME with parallel processing disabled"
-FSLPARALLEL=0; export FSLPARALLEL
-time trac-all -bedp -c ~/git/tr_accel/dmrirc.rta
-COMMENTOUT1
-echo "fsl_sub running $COMMANDNAME with parallel processing enabled"
+#trac-all -prep -c ~/git/tr_accel/dmrirc.rta
+#cp -r ~/freesurfer_rta/reconall2020_temp ~/freesurfer_rta/reconall2020_prep
+
+cp -r ~/freesurfer_rta/reconall2020_prep ~/freesurfer_rta/reconall2020_temp
 FSLPARALLEL=1; export FSLPARALLEL
-time trac-all -bedp -c ~/git/tr_accel/dmrirc.rta
-#time bedpostx $BEDPOSTDIR
+echo "fsl_sub running $COMMANDNAME with parallel processing enabled(FSLPARALLEL=$FSLPARALLEL)"
+time trac-all -bedp -c ~/git/tr_accel/dmrirc.rta>~/temp.txt
+cp -r ~/freesurfer_rta/reconall2020_temp ~/freesurfer_rta/reconall2020_enb_bedp
+time trac-all -path -c ~/git/tr_accel/dmrirc.rta>>~/temp.txt
+mv ~/freesurfer_rta/reconall2020_temp ~/freesurfer_rta/reconall2020_enb_path
+
+cp -r ~/freesurfer_rta/reconall2020_prep ~/freesurfer_rta/reconall2020_temp
+FSLPARALLEL=0; export FSLPARALLEL
+echo "fsl_sub running $COMMANDNAME with parallel processing disabled(FSLPARALLEL=$FSLPARALLEL)"
+time trac-all -bedp -c ~/git/tr_accel/dmrirc.rta>>~/temp.txt
+cp -r ~/freesurfer_rta/reconall2020_temp ~/freesurfer_rta/reconall2020_dis_bedp
+time trac-all -path -c ~/git/tr_accel/dmrirc.rta>>~/temp.txt
+mv ~/freesurfer_rta/reconall2020_temp ~/freesurfer_rta/reconall2020_dis_path
 
