f320ecb566daafd615935d2fa842fce42db5e20d
s1613134 <s1613134@u.tsukuba.ac.jp>
2020-02-05 18:56:25 +0900

    new file

---
+++ longitudinaall.sh
@@ -0,0 +1,32 @@
+#!/bun/bash
+set -x # debug mode
+
+# users definition
+FRSURFER_SCRIPTSDIR=~/git/fs-scripts
+FRSURFER_SCRIPTNAME=$FRSURFER_SCRIPTSDIR/fs_longitudinal.sh
+BASELONG_ID_REGEXP=^U2800[234]
+
+maxrunning=$(getconf _NPROCESSORS_ONLN)
+
+for fid in $(ls |grep "$BASELONG_ID_REGEXP"|sed -e "s/_MR[12]$//"|sort|uniq); 
+do 
+	running=$(ps -aux | grep "$FRSURFER_SCRIPTNAME" | wc -l)
+	while [ $running -gt $maxrunning ];
+	do
+		sleep 60
+		running=$(ps -aux | grep "$FRSURFER_SCRIPTNAME" | wc -l)
+	done
+	$FRSURFER_SCRIPTNAME $fid &
+done
+
+wait
+
+# QC base
+#$FRSURFER_SCRIPTSDIR/fs_qc_base.sh <base fsid>
+
+# QC long
+#$FRSURFER_SCRIPTSDIR/fs_qc_long.sh <base fsid>
+#for fid in $(ls |grep "$BASELONG_ID_REGEXP"|sed -e "s/_MR[12]$//"|sort|uniq); do $FRSURFER_SCRIPTSDIR/fs_qc_long.sh $fid; done
+
+echo "end of longitudinaall.sh"
+
