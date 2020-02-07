120111bae944a4a0e1ecbb773e9449065821f10f
s1613134 <s1613134@u.tsukuba.ac.jp>
2020-02-07 10:15:39 +0900

    update

--- longitudinaall.sh
+++ longitudinaall.sh
@@ -2,13 +2,13 @@
 set -x # debug mode
 
 # users definition
-FRSURFER_SCRIPTSDIR=~/git/fs-scripts
-FRSURFER_SCRIPTNAME=$FRSURFER_SCRIPTSDIR/fs_longitudinal.sh
-BASELONG_ID_REGEXP=^U2800[234]
+BASELONG_ID_REGEXP=^U2800[12]_[12]$
+
+FRSURFER_SCRIPTNAME=/usr/local/freesurfer/bin/recon-all
 
 maxrunning=$(getconf _NPROCESSORS_ONLN)
 
-for fid in $(ls |grep "$BASELONG_ID_REGEXP"|sed -e "s/_MR[12]$//"|sort|uniq); 
+for fid in $(ls |grep "$BASELONG_ID_REGEXP"|sed -e "s/_[12]$//"|sort|uniq); 
 do 
 	running=$(ps -aux | grep "$FRSURFER_SCRIPTNAME" | wc -l)
 	while [ $running -gt $maxrunning ];
@@ -16,17 +16,18 @@ do
 		sleep 60
 		running=$(ps -aux | grep "$FRSURFER_SCRIPTNAME" | wc -l)
 	done
-	$FRSURFER_SCRIPTNAME $fid &
+#	$FRSURFER_SCRIPTNAME $fid &
+	{ $FRSURFER_SCRIPTNAME -base $fid -tp ${fid}_1 -tp ${fid}_2 -all;$FRSURFER_SCRIPTNAME -long ${fid}_1 $fid -all;$FRSURFER_SCRIPTNAME -long ${fid}_2 $fid -all; }&
 done
 
 wait
 
 # QC base
-#$FRSURFER_SCRIPTSDIR/fs_qc_base.sh <base fsid>
+#~/git/fs-scripts/fs_qc_base.sh <base fsid>
 
 # QC long
-#$FRSURFER_SCRIPTSDIR/fs_qc_long.sh <base fsid>
-#for fid in $(ls |grep "$BASELONG_ID_REGEXP"|sed -e "s/_MR[12]$//"|sort|uniq); do $FRSURFER_SCRIPTSDIR/fs_qc_long.sh $fid; done
+#~/git/fs-scripts/fs_qc_long.sh <base fsid>
+#for fid in $(ls |grep "$BASELONG_ID_REGEXP"|sed -e "s/_[12]$//"|sort|uniq); do ~/git/fs-scripts/fs_qc_long.sh $fid; done
 
 echo "end of longitudinaall.sh"
 
