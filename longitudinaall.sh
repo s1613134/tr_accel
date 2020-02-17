6e6f23d49bbec0ed033fc849959757deb5f35406
s1613134 <s1613134@u.tsukuba.ac.jp>
2020-02-17 11:31:03 +0900

    update

--- longitudinaall.sh
+++ longitudinaall.sh
@@ -23,10 +23,25 @@ wait
 
 # QC base
 #~/git/fs-scripts/fs_qc_base.sh <base fsid>
+#for fid in $(ls |grep "$BASELONG_ID_REGEXP"|sed -e "s/_[12]$//"|sort|uniq); do ~/git/fs-scripts/fs_qc_base.sh $fid; done
 
 # QC long
 #~/git/fs-scripts/fs_qc_long.sh <base fsid>
 #for fid in $(ls |grep "$BASELONG_ID_REGEXP"|sed -e "s/_[12]$//"|sort|uniq); do ~/git/fs-scripts/fs_qc_long.sh $fid; done
+<<COMMENTOUT
+for fid in $(ls |grep "$BASELONG_ID_REGEXP"|sed -e "s/_[12]$//"|sort|uniq); do freeview \
+ -v ${fid}_1.long.${fid}/mri/norm.mgz \
+    ${fid}_2.long.${fid}/mri/norm.mgz \
+ -f ${fid}_1.long.${fid}/surf/lh.pial:edgecolor=red \
+    ${fid}_1.long.${fid}/surf/rh.pial:edgecolor=red \
+    ${fid}_1.long.${fid}/surf/lh.white:edgecolor=blue \
+    ${fid}_1.long.${fid}/surf/rh.white:edgecolor=blue \
+ -f ${fid}_2.long.${fid}/surf/lh.pial:edgecolor=pink \
+    ${fid}_2.long.${fid}/surf/rh.pial:edgecolor=pink \
+    ${fid}_2.long.${fid}/surf/lh.white:edgecolor=lightblue \
+    ${fid}_2.long.${fid}/surf/rh.white:edgecolor=lightblue \
+ -layout 1 -viewport coronal -zoom 2; done
+COMMENTOUT
 
 echo "end of longitudinaall.sh"
 
