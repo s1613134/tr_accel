9b6c28a3364e5685c5733f4eb00863cf7a002366
s1613134 <s1613134@u.tsukuba.ac.jp>
2020-02-26 12:16:48 +0900

    auto retry

--- fs_autotrac_parallel.sh
+++ fs_autotrac_parallel.sh
@@ -9,6 +9,15 @@
 
 #set -x #for debugging
 
+# definition in late dmrirc file
+# Output directory where trac-all results will be saved
+set dtroot = $SUBJECTS_DIR/tracula # trallall_outputs
+#set subjlist = (hogehogeid) # use -s option!
+set dcmroot = $SUBJECTS_DIR # /mnt/data/synapsology
+#set dcmlist = (dcmhogehoge) # use -i option!
+set bvecfile = $SUBJECTS_DIR/bvecs.txt
+set bvalfile = $SUBJECTS_DIR/bvals.txt
+
 #Check OS
 os=$(uname)
 
@@ -36,10 +45,10 @@ fi
 
 
 #Check if the files are specified
-if [ $# -lt 2 ]
+if [ $# -lt 1 ]
 then
-  echo "Please specify subjectlist and dmrirc file!"
-  echo "Usage: $0 <subjectlist> <dmrirc>"
+  echo "Please specify subjectlist"
+  echo "Usage: $0 <subjectlist>"
   exit 1
 fi
 
@@ -69,7 +78,7 @@ done
 #Confirm subjectlist and dmrirc
 while true
 do
-  echo "subjectlist is ${1} and dmrirc is ${2}."
+  echo "subjectlist is ${1}."
   echo "Are they correct (yes/no)?"
 
   read answer
@@ -89,26 +98,51 @@ do
 done
 
 
-#trac-all -prep
+#trac-all -prep -bedp -path
 fsid_list=""
 cat $1 | while read fsid dwi
 do
   running=$(ps -aux | grep 'bin/trac-all' | wc -l)
   while [ $running -gt $maxrunning ];
   do
-    sleep 600
+    sleep 60
     running=$(ps -aux | grep 'bin/trac-all' | wc -l)
   done
-  { trac-all -prep -c $2 -i $dwi -s $fsid ;\
-    trac-all -bedp -c $2 -s $fsid ;\
-    trac-all -path -c $2 -s $fsid ; } &
+  { trac-all -prep -i $dwi -s $fsid ;\
+    trac-all -bedp -i $dwi -s $fsid ;\
+    trac-all -path -s $fsid ; } &
   fsid_list=${fsid_list}" "$fsid
   echo "fsid_list is "${fsid_list} 
 done
 
-wait
+# auto retry
+dtrootdepth=$(echo ${dtroot}|sed -e "s@/@ @g"|wc -w)
+grep "trac-paths exited with ERRORS" ${dtroot}/U280*/scripts/trac-all.log|while read templine;do set ${templine//\//  };echo ${$(expr ${dtrootdepth} + 1)};done|sort|uniq>${dtroot}/trac_path_retrylist.txt
+trac_path_retryfidnum=$(cat ${dtroot}/trac_path_retrylist.txt|wc -l)
+while [ $trac_path_retryfidnum -gt 0 ];
+do
+	cat ${dtroot}/trac_path_retrylist.txt| while read fsid
+	do
+	  running=$(ps -aux | grep 'bin/trac-all' | wc -l)
+	  while [ $running -gt $maxrunning ];
+	  do
+	    sleep 60
+	    running=$(ps -aux | grep 'bin/trac-all' | wc -l)
+	  done # while [ $running -gt $maxrunning ];
+	  mv -b ${dtroot}/${fsid}/scripts/trac-all.log ${dtroot}/${fsid}/scripts/trac-all.log.old
+	  dwi=D_${fsid}_PA.nii
+	  { trac-all -prep -i $dwi -s $fsid ;\
+	    trac-all -bedp -i $dwi -s $fsid ;\
+	    trac-all -path -s $fsid ; } &
+	done # while read fsid
+	grep "trac-paths exited with ERRORS" ${dtroot}/U280*/scripts/trac-all.log|while read templine;do set ${templine//\//  };echo ${$(expr ${dtrootdepth} + 1)};done|sort|uniq>${dtroot}/trac_path_retrylist.txt
+	trac_path_retryfidnum=$(cat ${dtroot}/trac_path_retrylist.txt|wc -l)
+done # while [ $trac_path_retryfidnum -gt 0 ];
 
-trac-all -stat -c $2 -s $fsid_list
+wait
+trac-all -stat -s $fsid_list
+
+echo "fsid_list is "${fsid_list} 
 echo "the end of fs_autotrac_parallel"
 
 exit
